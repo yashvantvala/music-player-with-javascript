09f080ed87aac13a2e55758a8c66c33fd0c9c4f3
Yashvant Vala <yashvantvala19899@gmail.com>
2021-04-10 23:21:46 +0530

    Created simple music player with javascript

---
+++ script.js
@@ -0,0 +1,86 @@
+const musicContainer  = document.querySelector('.music-container'); 
+const playBtn  = document.querySelector('#play'); 
+const prevBtn  = document.querySelector('#prev'); 
+const nextBtn  = document.querySelector('#next'); 
+const audio  = document.querySelector('#audio'); 
+const progress  = document.querySelector('.progress'); 
+const progressContainer  = document.querySelector('.progress-container'); 
+const title  = document.querySelector('#title'); 
+const cover  = document.querySelector('#cover'); 
+
+//song titles
+
+const songs = ['first','second','third'];
+
+//keep track of songs
+
+let songIndex = 2;
+
+loadSong(songs[songIndex]);
+
+//update song details
+
+function loadSong(song){
+	title.innerText = song;
+	audio.src = `music/${song}.mp3`;
+	cover.src = `images/${song}.png`;
+}
+function playSong(){
+	musicContainer.classList.add('play');
+	playBtn.querySelector('i.fa').classList.remove('play');
+	playBtn.querySelector('i.fa').classList.add('fa-pause');
+	audio.play();
+}
+function pauseSong(){
+	musicContainer.classList.remove('play');
+	playBtn.querySelector('i.fa').classList.add('play');
+	playBtn.querySelector('i.fa').classList.remove('fa-pause');
+	audio.pause();
+}
+function prevSong(){
+	songIndex--;
+	if(songIndex<0){
+		songIndex = songs.length-1;
+	}
+	loadSong(songs[songIndex]);
+	playSong();
+}
+function nextSong(){
+	songIndex++;
+	if(songIndex>songs.length-1){
+		songIndex = 0;
+	}
+	loadSong(songs[songIndex]);
+	playSong();
+}
+function updateProgress(e){
+	const { duration, currentTime } = e.srcElement;
+	const progressPercent = (currentTime/duration)*100;
+	progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e){
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const duration = audio.duration;
+
+	audio.currentTime = (clickX/width)*duration;
+}
+playBtn.addEventListener('click', ()=>{
+	const isPlaying = musicContainer.classList.contains('play');
+
+	if(isPlaying){
+		pauseSong();
+	}else{
+		playSong();
+	}
+})
+
+prevBtn.addEventListener('click',prevSong);
+nextBtn.addEventListener('click',nextSong);
+
+audio.addEventListener('timeupdate',updateProgress)
+
+progressContainer.addEventListener('click',setProgress)
+
+audio.addEventListener('ended',nextSong)
